146abb354a308a2c466856c8861242f6a848f3bf
huntams <dissirina2904@gmail.com>
2023-03-01 13:31:39 +0700

    2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Player player = new Player();
         Monster monster = null;
-        boolean End = false;
         boolean StatusCreated = false;
         while (!StatusCreated) {//Создание игрока
             StatusCreated = player.CreateCreature();
@@ -20,7 +16,7 @@ public class Main {
         }
         while (true) {//Пока одно из существ не умрёт
             monster.AttackEnemy(player);
-            if (player.MaxHP / 2 > player.getHP() && player.getHP() > 0)//Восстановление здоровье
+            if (player.getMaxHP() / 2 > player.getHP() && player.getHP() > 0)//Восстановление здоровье
                 player.RestoreHP();
             if (player.DeadCreature()) {
                 System.out.println("Конец игры,монстр победил");
